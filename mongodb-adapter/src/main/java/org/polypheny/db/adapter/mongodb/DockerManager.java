6a3991e7685e0c32b7e4dafdea4276ab6ad2d442
datomo <dave.lengw@gmail.com>
2021-03-06 17:09:48 +0100

    added barebone dockerManager

---
+++ mongodb-adapter/src/main/java/org/polypheny/db/adapter/mongodb/DockerManager.java
@@ -0,0 +1,275 @@
+/*
+ * Copyright 2019-2021 The Polypheny Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.polypheny.db.adapter.mongodb;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.CreateContainerCmd;
+import com.github.dockerjava.api.command.PullImageResultCallback;
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.Ports;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
+import com.google.common.collect.ImmutableList;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import lombok.Getter;
+
+// TODO DL: check if already existing containers in system etc.
+// and move to core
+
+
+/**
+ * The class servers as a organization unit which controls all Docker Containers in Polypheny.
+ * While the interacting classes mostly interact with the underlying Containers,
+ * this instance is used to have a control layer, which allows to restore, start or shutdown multiple of
+ * these instances at the same time.
+ */
+public class DockerManager {
+
+    private final DockerClient client;
+    private static DockerManager INSTANCE;
+    private final HashMap<String, Container> availableContainers = new HashMap<>();
+    private final HashMap<Integer, List<String>> containersOnAdapter = new HashMap<>();
+
+
+    public static DockerManager getInstance() {
+        if ( INSTANCE == null ) {
+            INSTANCE = new DockerManager();
+        }
+        return INSTANCE;
+    }
+
+
+    private DockerManager() {
+        // TODO: check if docker running or else do not show?
+        client = this.generateClient();
+    }
+
+
+    private DockerClient generateClient() {
+        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                .withDockerHost( "tcp://localhost:2375" )
+                //.withDockerTlsVerify( true ) //TODO DL: use certificates
+                //.withDockerCertPath(certPath)
+                .build();
+
+        DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
+                .dockerHost( config.getDockerHost() )
+                .sslConfig( config.getSSLConfig() )
+                .build();
+
+        DockerClient client = DockerClientImpl.getInstance( config, httpClient );
+        client.pingCmd().exec();
+        return client;
+    }
+
+
+    protected boolean checkIfUnique( String uniqueName ) {
+        return !availableContainers.containsKey( uniqueName );
+    }
+
+
+    private void registerIfAbsent( Container container ) {
+        if ( !availableContainers.containsKey( container.uniqueName ) ) {
+            availableContainers.put( container.uniqueName, container );
+
+            if ( !containersOnAdapter.containsKey( container.adapterId ) ) {
+                containersOnAdapter.put( container.adapterId, ImmutableList.of( container.uniqueName ) );
+            } else {
+                List<String> containerNames = new ArrayList<>( containersOnAdapter.get( container.adapterId ) );
+                containerNames.add( container.uniqueName );
+                containersOnAdapter.put( container.adapterId, ImmutableList.copyOf( containerNames ) );
+            }
+        }
+    }
+
+
+    public Container createContainer( String uniqueName, int adapterId, ContainerImage image, int port ) {
+        Container container = new Container( adapterId, uniqueName, image, Collections.singletonMap( image.internalPort, port ) );
+        registerIfAbsent( container );
+        return container;
+    }
+
+
+    private void start( Container container ) {
+        registerIfAbsent( container );
+
+        if ( container.status != ContainerStatus.PREPARED ) {
+            prepare( container );
+        }
+
+        Ports bindings = new Ports();
+
+        for ( Map.Entry<Integer, Integer> mapping : container.internalExternalPortMapping.entrySet() ) {
+            bindings.bind( ExposedPort.tcp( mapping.getKey() ), Ports.Binding.bindPort( mapping.getValue() ) );
+        }
+
+        CreateContainerCmd cmd = client.createContainerCmd( container.type.getFullName() )
+                .withName( container.uniqueName )
+                .withExposedPorts( container.getExposedPorts() );
+
+        Objects.requireNonNull( cmd.getHostConfig() ).withPortBindings( bindings );
+        cmd.exec();
+
+        client.startContainerCmd( container.uniqueName ).exec();
+
+        container.setStatus( ContainerStatus.RUNNING );
+    }
+
+
+    private void prepare( Container container ) {
+
+        registerIfAbsent( container );
+
+        client.pullImageCmd( container.type.getFullName() ).exec( container.callback );
+
+        // TODO: blocking for now, maybe change or show warning?
+        try {
+            container.callback.awaitCompletion();
+        } catch ( InterruptedException e ) {
+            throw new RuntimeException( "The downloading of the image for  " + container.uniqueName + " failed." );
+        }
+        container.setStatus( ContainerStatus.PREPARED );
+    }
+
+
+    public void shutdownAll( int adapterId ) {
+        containersOnAdapter.get( adapterId ).forEach( containerName -> availableContainers.get( containerName ).shutdown() );
+    }
+
+
+    private void shutdown( Container container ) {
+        client.stopContainerCmd( container.uniqueName ).exec();
+        client.removeContainerCmd( container.uniqueName ).exec();
+    }
+
+
+    /**
+     * This enum unifies the name building and providing for ContainerImages
+     */
+    public enum ContainerImage {
+        MONGODB( "mongo", 27017 );
+
+        @Getter
+        private final String name;
+        @Getter
+        private final String version;
+        @Getter
+        private final int internalPort;
+
+
+        public String getFullName() {
+            return this.name + ":" + this.version;
+        }
+
+
+        ContainerImage( String name, int internalPort, String version ) {
+            this.name = name;
+            this.version = version;
+            this.internalPort = internalPort;
+        }
+
+
+        ContainerImage( String name, int internalPort ) {
+            this.name = name;
+            this.version = "latest";
+            this.internalPort = internalPort;
+        }
+
+    }
+
+
+    public enum ContainerStatus {
+        INIT,
+        PREPARED,
+        RUNNING,
+        ERROR;
+    }
+
+
+    /**
+     * The container is the main interaction instance for calling classes when interacting with Docker.
+     * It holds all information for a specific Container
+     */
+    public static class Container {
+
+        private final ContainerImage type;
+        private final PullImageResultCallback callback;
+        private final String uniqueName;
+        private final Map<Integer, Integer> internalExternalPortMapping;
+        private final int adapterId;
+        private ContainerStatus status;
+
+
+        private Container(
+                int adapterId,
+                String uniqueName,
+                ContainerImage image,
+                Map<Integer, Integer> internalExternalPortMapping
+        ) {
+            if ( !getInstance().checkIfUnique( uniqueName ) ) {
+                throw new RuntimeException( "The name for the Docker container is already taken." );
+            }
+            this.adapterId = adapterId;
+            this.type = image;
+            this.uniqueName = uniqueName;
+            this.internalExternalPortMapping = internalExternalPortMapping;
+            this.callback = new PullImageResultCallback();
+            this.status = ContainerStatus.INIT;
+        }
+
+
+        public Container prepare() {
+            getInstance().prepare( this );
+
+            return this;
+        }
+
+
+        public Container start() {
+            getInstance().start( this );
+
+            return this;
+        }
+
+
+        public void shutdown() {
+            getInstance().shutdown( this );
+        }
+
+
+        public List<ExposedPort> getExposedPorts() {
+            return internalExternalPortMapping.values().stream().map( ExposedPort::tcp ).collect( Collectors.toList() );
+        }
+
+
+        private void setStatus( ContainerStatus status ) {
+            this.status = status;
+        }
+
+    }
+
+
+}
--- mongodb-adapter/src/main/java/org/polypheny/db/adapter/mongodb/MongoStore.java
+++ mongodb-adapter/src/main/java/org/polypheny/db/adapter/mongodb/MongoStore.java
@@ -16,23 +16,13 @@
 
 package org.polypheny.db.adapter.mongodb;
 
-import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.command.CreateContainerCmd;
-import com.github.dockerjava.api.command.PullImageResultCallback;
-import com.github.dockerjava.api.exception.ConflictException;
-import com.github.dockerjava.api.model.ExposedPort;
-import com.github.dockerjava.api.model.Ports;
-import com.github.dockerjava.core.DefaultDockerClientConfig;
-import com.github.dockerjava.core.DockerClientConfig;
-import com.github.dockerjava.core.DockerClientImpl;
-import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
-import com.github.dockerjava.transport.DockerHttpClient;
 import com.google.common.collect.ImmutableList;
 import com.mongodb.MongoClient;
 import java.util.List;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.polypheny.db.adapter.DataStore;
+import org.polypheny.db.adapter.mongodb.DockerManager.ContainerImage;
 import org.polypheny.db.catalog.Catalog;
 import org.polypheny.db.catalog.entity.CatalogColumn;
 import org.polypheny.db.catalog.entity.CatalogColumnPlacement;
@@ -43,7 +33,6 @@ import org.polypheny.db.schema.Schema;
 import org.polypheny.db.schema.SchemaPlus;
 import org.polypheny.db.schema.Table;
 import org.polypheny.db.transaction.PolyXid;
-import org.polypheny.db.util.FileSystemManager;
 
 @Slf4j
 public class MongoStore extends DataStore {
@@ -60,69 +49,24 @@ public class MongoStore extends DataStore {
             new AdapterSettingInteger( "port", false, true, false, 27017 )
     );
     private final MongoClient connection;
-
-    private DockerClient client;
     private MongoSchema currentSchema;
-    private String databaseName = "test_db";
 
 
     public MongoStore( int adapterId, String uniqueName, Map<String, String> settings ) {
         super( adapterId, uniqueName, settings, Boolean.parseBoolean( settings.get( "persistent" ) ) );
-        FileSystemManager fileManger = FileSystemManager.getInstance();
-        //String certPath = fileManger.registerNewFolder("certs").getAbsolutePath();
-
-        // TODO: check if docker running or else do not show?
-        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
-                .withDockerHost( "tcp://localhost:2375" )
-                //.withDockerTlsVerify(true)
-                //.withDockerCertPath(certPath)
-                .build();
-
-        DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
-                .dockerHost( config.getDockerHost() )
-                .sslConfig( config.getSSLConfig() )
-                .build();
-
-        client = DockerClientImpl.getInstance( config, httpClient );
-        client.pingCmd().exec();
-
-        PullImageResultCallback callback = new PullImageResultCallback();
-
-        client.pullImageCmd( "mongo:latest" ).exec( callback );
-        try {
-            // TODO: blocking for now, maybe change or show warning?
-            callback.awaitCompletion();
-            log.warn( "finished" );
-
-            generateContainer( uniqueName, settings );
-        } catch ( InterruptedException | ConflictException e ) {
-            e.printStackTrace();
-        }
+        DockerManager.getInstance()
+                .createContainer(
+                        getUniqueName(),
+                        getAdapterId(),
+                        ContainerImage.MONGODB,
+                        Integer.parseInt( settings.get( "port" ) ) )
+                .prepare()
+                .start();
 
         addInformationPhysicalNames();
         enableInformationPage();
 
         this.connection = new MongoClient( "localhost", Integer.parseInt( settings.get( "port" ) ) );
-    }
-
-
-    private void generateContainer( String uniqueName, Map<String, String> settings ) {
-        /* TODO DL: solution when container name already exist
-        if (client.listContainersCmd().exec().stream().anyMatch(container -> container.getNames()[0].contains(uniqueName))) {
-            generateContainer(uniqueName+"_1", settings);
-            return;
-        }*/
-        int port = Integer.parseInt( settings.get( "port" ) );
-        Ports bindings = new Ports();
-        bindings.bind( ExposedPort.tcp( port ), Ports.Binding.bindPort( port ) );
-        CreateContainerCmd cmd = client.createContainerCmd( "mongo" )
-                .withName( uniqueName )
-                .withExposedPorts( ExposedPort.tcp( port ) );
-
-        cmd.getHostConfig().withPortBindings( bindings );
-        cmd.exec();
-
-        client.startContainerCmd( uniqueName ).exec();
     }
 
 
@@ -185,8 +129,7 @@ public class MongoStore extends DataStore {
 
     @Override
     public void shutdown() {
-        client.stopContainerCmd( getUniqueName() ).exec();
-        client.removeContainerCmd( getUniqueName() ).exec();
+        DockerManager.getInstance().shutdownAll( getAdapterId() );
 
         removeInformationPage();
     }
